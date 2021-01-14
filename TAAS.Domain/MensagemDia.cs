0a456c21d910d63619ecac3fb79df0e2d5cdd61e
Márcio Oliveira Corbolan <marciocorbolan@hotmail.com>
2021-01-13 21:39:27 -0300

    Criação da camada domain e repository

---
+++ TAAS.Domain/MensagemDia.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TAAS.Domain
+{
+    public class MensagemDia
+    {
+        public int Id { get; set; }
+        public string Texto { get; set; }
+        public bool Exibida { get; set; }
+        public DateTime DtCadastro { get; set; }
+        public DateTime DtModificado { get; set; }
+    }
+}
---
+++ TAAS.Repository/TaasContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using TAAS.Domain;
+
+namespace TAAS.Repository
+{
+    public class TaasContext : DbContext
+    {
+        public TaasContext(DbContextOptions<TaasContext> opt) : base(opt) {}
+
+        public DbSet<MensagemDia> MensagesDias { get; set; }
+    }
+}
